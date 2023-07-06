a60e348533be526f8f110d78ed5a79889d4ede37
gzolbolotova <67222280+gzolbolotova@users.noreply.github.com>
2023-07-06 23:09:00 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -10,90 +10,90 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final String users = "users";
-    Connection connection = Util.getConnection();
+private final String users = "users";
+Connection connection = Util.getConnection();
 
-    public UserDaoJDBCImpl() {
+public UserDaoJDBCImpl() {
+
+}
+
+public void createUsersTable() {
+    String sqlCommand = String.format("CREATE TABLE IF NOT EXISTS %s (id INT PRIMARY KEY AUTO_INCREMENT, " +
+            "name VARCHAR(20), lastName VARCHAR(25), age INT)", users);
+    try (ResultSet resultSet = connection.createStatement().executeQuery(String.format("SHOW TABLES FROM bdforpp LIKE '%s'", users))){
+        if(resultSet.next()){
+            System.out.println("Exists table");
+        }
+        else {
+            connection.createStatement().execute(sqlCommand);
+        }
+    } catch (SQLException ignored) {
 
     }
 
-    public void createUsersTable() throws SQLException {
-        String sqlCommand = String.format("CREATE TABLE IF NOT EXISTS %s (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), lastName VARCHAR(25), age INT)", users);
-        try(Statement statement = connection.createStatement()){
-            statement.execute(sqlCommand);
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+}
 
+public void dropUsersTable() {
+    String sqlCom = String.format("DROP TABLE IF EXISTS %s", users);
+    try {
+        connection.createStatement().executeUpdate(sqlCom);
+    } catch (SQLException ignored) {
     }
 
-    public void dropUsersTable() {
-        String sqlCom = String.format("DROP TABLE %s", users);
-        try {
-            connection.createStatement().executeUpdate(sqlCom);
-        } catch (SQLException e) {
-        }
+}
 
+public void saveUser(String name, String lastName, byte age) {
+    String save = String.format("INSERT INTO %s (name, lastName, age) VALUES(?, ?, ?)", users);
+    try (PreparedStatement preparedStatement = connection.prepareStatement(save)) {
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, lastName);
+        preparedStatement.setByte(3, age);
+        preparedStatement.executeUpdate();
+    } catch (SQLException ignored) {
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-        String save = String.format("INSERT INTO %s (name, lastName, age) VALUES(?, ?, ?)", users);
-        try (PreparedStatement preparedStatement = connection.prepareStatement(save)){
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+}
 
+public void removeUserById(long id) {
+    String sql = String.format("DELETE FROM %s WHERE id = ?", users);
+    try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        preparedStatement.setLong(1, id);
+        preparedStatement.executeUpdate();
+    } catch (SQLException ignored) {
     }
 
-    public void removeUserById(long id) {
-        //User user = new User();
-        String sql = String.format("DELETE FROM %s WHERE id = ?", users);
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+}
+
+public List<User> getAllUsers() {
+    List<User> userList = new ArrayList<>();
+    String sql = String.format("SELECT * FROM %s", users);
+    try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            long id = resultSet.getLong("ID");
+            String name = resultSet.getString("NAME");
+            String lastName = resultSet.getString("LASTNAME");
+            Byte age = resultSet.getByte("AGE");
+
+            User user = new User();
+            user.setId(id);
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
+            userList.add(user);
         }
 
+    } catch (SQLException ignored) {
     }
+    return userList;
 
-    public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<>();
-        String sql = String.format("SELECT * FROM %s", users);
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
-                long id = resultSet.getLong("ID");
-                String name = resultSet.getString("NAME");
-                String lastName = resultSet.getString("LASTNAME");
-                Byte age = resultSet.getByte("AGE");
+}
 
-                User user = new User();
-                user.setId(id);
-                user.setName(name);
-                user.setLastName(lastName);
-                user.setAge(age);
-                userList.add(user);
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return userList;
-
+public void cleanUsersTable() {
+    String sql = String.format("TRUNCATE TABLE %s", users);
+    try (Statement statement = connection.createStatement()) {
+        statement.executeUpdate(sql);
+    } catch (SQLException ignored) {
     }
-
-
-        public void cleanUsersTable() {
-        String sql = String.format("TRUNCATE TABLE %s", users);
-        try (Statement statement = connection.createStatement()){
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        }
+}
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,14 +7,16 @@ import java.sql.Statement;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private  static final String URL = "jdbc:mysql://localhost:3306/bdforpp";
+    private static final String USER = "root1";
+    private static final String PASSWORD = "root1";
     public static Connection getConnection(){
         Connection connection = null;
         try {
-            final String url = "jdbc:mysql://localhost:3306/bdforpp";
-            final String user = "root1";
-            final String password = "root1";
+
+
             connection= DriverManager.getConnection(
-                    url,user,password);
+                    URL,USER,PASSWORD);
            //System.out.println("Подключено");
         } catch (SQLException e) {
             throw new RuntimeException(e);
